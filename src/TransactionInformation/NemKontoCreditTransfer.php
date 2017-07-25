2ddce41b08e01f02cf324e7e619d2af360e488ec
Troels Lenda <troelslenda@gmail.com>
2017-07-25 15:34:48 +0200

    Adding NemKonto credit transfer type

---
+++ src/TransactionInformation/NemKontoCreditTransfer.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace NordeaPayment\TransactionInformation;
+
+use DOMDocument;
+use InvalidArgumentException;
+use NordeaPayment\BIC;
+use NordeaPayment\FinancialInstitutionInterface;
+use NordeaPayment\IBAN;
+use NordeaPayment\IID;
+use NordeaPayment\Money;
+use NordeaPayment\PaymentInformation\PaymentInformation;
+use NordeaPayment\PostalAddressInterface;
+
+/**
+ * BankCreditTransfer contains all the information about a type 3 transaction.
+ */
+class NemKontoCreditTransfer extends CreditTransfer
+{
+    /**
+     * @var IBAN
+     */
+    protected $creditorIBAN;
+
+    /**
+     * @var FinancialInstitutionInterface
+     */
+    protected $creditorAgent;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param IBAN    $creditorIBAN  IBAN of the creditor
+     * @param BIC|IID $creditorAgent BIC or IID of the creditor's financial institution
+     * @param string $cpr The creditors CPR number.
+     *
+     * @throws \InvalidArgumentException When the amount is not in EUR or CHF or when the creditor agent is not BIC or IID.
+     */
+    public function __construct($instructionId, $endToEndId, Money\Money $amount, $creditorName, PostalAddressInterface $creditorAddress, IBAN $creditorIBAN, FinancialInstitutionInterface $creditorAgent, $cpr)
+    {
+        if (!$amount instanceof Money\EUR && !$amount instanceof Money\DKK) {
+            throw new InvalidArgumentException(sprintf(
+              'The amount must be an instance of Money\EUR or Money\CHF (instance of %s given).',
+              get_class($amount)
+            ));
+        }
+
+        if (!$creditorAgent instanceof BIC && !$creditorAgent instanceof IID) {
+            throw new InvalidArgumentException('The creditor agent must be an instance of BIC or IID.');
+        }
+
+        parent::__construct($instructionId, $endToEndId, $amount, $creditorName, $creditorAddress);
+
+        $this->creditorIBAN = $creditorIBAN;
+        $this->creditorAgent = $creditorAgent;
+        $this->serviceLevel = 'NURG';
+        $this->cpr = $cpr;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function asDom(DOMDocument $doc, PaymentInformation $paymentInformation)
+    {
+        $root = $this->buildHeader($doc, $paymentInformation);
+
+        $creditorAgent = $doc->createElement('CdtrAgt');
+        $creditorAgent->appendChild($this->creditorAgent->asDom($doc));
+        $root->appendChild($creditorAgent);
+
+        $root->appendChild($this->buildNemKontoCreditor($doc, $this->cpr));
+
+        $creditorAccount = $doc->createElement('CdtrAcct');
+        $creditorAccount->appendChild($this->creditorIBAN->asDom($doc));
+        $root->appendChild($creditorAccount);
+
+        $this->appendPurpose($doc, $root);
+
+        $this->appendRemittanceInformation($doc, $root);
+
+        return $root;
+    }
+
+    protected function buildNemKontoCreditor(\DOMDocument $doc, $cpr)
+    {
+        $code = $doc->createElement('Cd', 'SOSE');
+
+        $schemeName = $doc->createElement('SchmeNm');
+        $schemeName->appendChild($code);
+
+        $cpr = $doc->createElement('Id', $cpr);
+
+        $other = $doc->createElement('Othr');
+        $other->appendChild($cpr);
+        $other->appendChild($schemeName);
+
+        $privateId = $doc->createElement('PrvtId');
+        $privateId->appendChild($other);
+
+        $id = $doc->createElement('Id');
+        $id->appendChild($privateId);
+
+        $creditor = parent::buildCreditor($doc);
+        $creditor->appendChild($id);
+
+        return $creditor;
+    }
+}
+
